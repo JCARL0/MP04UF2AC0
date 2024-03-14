a2639d014df1a2c030b82d438a60f7afd46a3cb1
JCARL0 <john.magno@estudiant.fjaverianas.com>
2024-03-14 09:21:00 +0100

    Tarea 2

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+namespace RssParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rssFeedUrl = "https://techcrunch.com/feed/";
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(rssFeedUrl);
+            XmlNodeList nodeList = xmlDoc.GetElementsByTagName("item");
+            foreach (XmlNode node in nodeList)
+            {
+                string title = node["title"].InnerText;
+                Console.WriteLine("titulo: " + title);
+                string category = node["category"].InnerText;
+                Console.WriteLine("categoria: " + category);
+                string link = node["link"].InnerText;
+                Console.WriteLine("link: " + link);
+                string description = node["description"].InnerText;
+                Console.WriteLine("descripcion: " + description);
+                string pubDate = node["pubDate"].InnerText;
+                Console.WriteLine("lanzamiento: " + pubDate);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
